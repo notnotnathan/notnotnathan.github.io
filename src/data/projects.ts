7858d8136641d9ae0200a9c31c5d62f302650522
notnotnathan <61069762+notnotnathan@users.noreply.github.com>
2026-04-07 23:24:49 -0400

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -50,8 +50,7 @@ export const projects: ProjectData[] = [
     bullets: [
       "designed 12 dof quadruped using hobby rc servos.",
       "optimized design and tolerances for 3d printing and assembly.",
-      "currently working on custom 12 channel servo driver",
-    ],
+      "hierarchical control architecture using custom 12-channel stm32 driver for low-level servo control, raspberry pi for high-level perception, mapping, and motion planning, with lidar for environmental sensing
     blocks: [
       { type: "text", content: "work in progress." },
       { type: "images", images: [quadrupedImg1, quadrupedImg2], height: 300, gap: 20 },
